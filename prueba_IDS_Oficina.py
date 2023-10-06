90cfe981cbf513987a471b22bb9c7a1573f779a3
florencia <florenciadchoque@gmail.com>
2023-10-06 16:07:24 -0300

    agrego prueba_IDS_oficina

---
+++ prueba_IDS_Oficina.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  6 13:47:41 2023
+
+@author: Cibion
+"""
+
+from ids_peak import ids_peak
+from ids_peak_ipl import ids_peak_ipl
+import numpy as np
+import cv2
+
+
+
+
+def main():
+        # initialize library
+    ids_peak.Library.Initialize()
+
+    # create a device manager object
+    device_manager = ids_peak.DeviceManager.Instance()
+    print("Type Device: ", type(device_manager))
+    try:
+        # update the device manager
+        device_manager.Update()
+
+        # exit program if no device was found
+        if device_manager.Devices().empty():
+            print("No device was found. Exiting Program.")
+            return
+        #Open first available device
+        device = device_manager.Devices()[0].OpenDevice(ids_peak.DeviceAccessType_Control)
+        #Get the remote device node map
+        nodemap_remote_device= device.RemoteDevice().NodeMaps()[0]
+        
+        #Print model name and user ID
+        print("Opening camera model: "+ nodemap_remote_device.FindNode("DeviceModeName").Value())
+        
+        
