78f942905fddcf2b09f776836ebd0b98bd936abd
florencia <florenciadchoque@gmail.com>
2023-10-12 13:21:06 -0300

    Corrección de los errores al pasar desde Pyside6 a PyQt5

--- mainwindow_Pyqt5.py
+++ mainwindow_Pyqt5.py
@@ -49,7 +49,7 @@ class MainWindow(QtWidgets.QMainWindow):
         
         #Variables de instancia relacionadas con laadquisición de imágenes
         self.__display = None
-        self.__acquisition_timer = QTimer() #temporizador para controlar la frecuencia de adquisición,
+        self.__acquisition_timer = QtCore.QTimer() #temporizador para controlar la frecuencia de adquisición,
         self.__frame_counter = 0 #Contador del numero de cuadros
         self.__error_counter = 0 #Contador del numero de errores
         self.__acquisition_running = False #bandera para indicar si la adquisición está en curso.
@@ -67,9 +67,9 @@ class MainWindow(QtWidgets.QMainWindow):
                 self.__display = Display()
                 self.__layout.addWidget(self.__display)
                 if not self.__start_acquisition():
-                    QMessageBox.critical(self, "Unable to start acquisition!", QMessageBox.Ok)
+                    QtWidgets.QMessageBox.critical(self, "Unable to start acquisition!", QMessageBox.Ok)
             except Exception as e:
-                QMessageBox.critical(self, "Exception", str(e), QMessageBox.Ok)
+                QtWidgets.QMessageBox.critical(self, "Exception", str(e), QMessageBox.Ok)
 
         else:
             self.__destroy_all()
@@ -100,7 +100,7 @@ class MainWindow(QtWidgets.QMainWindow):
 
             # Return if no device was found
             if device_manager.Devices().empty():
-                QMessageBox.critical(self, "Error", "No device found!", QMessageBox.Ok)
+                QtWidgets.QMessageBox.critical(self, "Error", "No device found!", QtWidgets.QMessageBox.Ok)
                 return False
 
             # Open the first openable device in the managers device list
@@ -111,13 +111,13 @@ class MainWindow(QtWidgets.QMainWindow):
 
             # Return if no device could be opened
             if self.__device is None:
-                QMessageBox.critical(self, "Error", "Device could not be opened!", QMessageBox.Ok)
+                QtWidgets.QMessageBox.critical(self, "Error", "Device could not be opened!", QtWidgets.QMessageBox.Ok)
                 return False
 
             # Open standard data stream
             datastreams = self.__device.DataStreams()
             if datastreams.empty():
-                QMessageBox.critical(self, "Error", "Device has no DataStream!", QMessageBox.Ok)
+                QtWidgets.QMessageBox.critical(self, "Error", "Device has no DataStream!", QtWidgets.QMessageBox.Ok)
                 self.__device = None
                 return False
 
@@ -149,7 +149,7 @@ class MainWindow(QtWidgets.QMainWindow):
 
             return True
         except ids_peak.Exception as e:
-            QMessageBox.critical(self, "Exception", str(e), QMessageBox.Ok)
+            QtWidgets.QMessageBox.critical(self, "Exception", str(e), QtWidgets.QMessageBox.Ok)
 
         return False
 
@@ -166,7 +166,7 @@ class MainWindow(QtWidgets.QMainWindow):
                 for buffer in self.__datastream.AnnouncedBuffers():
                     self.__datastream.RevokeBuffer(buffer)
             except Exception as e:
-                QMessageBox.information(self, "Exception", str(e), QMessageBox.Ok)
+                QtWidgets.QMessageBox.information(self, "Exception", str(e), QtWidgets.QMessageBox.Ok)
 
     def __start_acquisition(self):
         """
@@ -189,7 +189,7 @@ class MainWindow(QtWidgets.QMainWindow):
             self.__nodemap_remote_device.FindNode("AcquisitionFrameRate").SetValue(target_fps)
         except ids_peak.Exception:
             # AcquisitionFrameRate is not available. Unable to limit fps. Print warning and continue on.
-            QMessageBox.warning(self, "Warning",
+            QtWidgets.QMessageBox.warning(self, "Warning",
                                 "Unable to limit fps, since the AcquisitionFrameRate Node is"
                                 " not supported by the connected camera. Program will continue without limit.")
 
@@ -242,27 +242,27 @@ class MainWindow(QtWidgets.QMainWindow):
                 try:
                     self.__nodemap_remote_device.FindNode("TLParamsLocked").SetValue(0)
                 except Exception as e:
-                    QMessageBox.information(self, "Exception", str(e), QMessageBox.Ok)
+                    QtWidgets.QMessageBox.information(self, "Exception", str(e), QtWidgets.QMessageBox.Ok)
 
         except Exception as e:
-            QMessageBox.information(self, "Exception", str(e), QMessageBox.Ok)
+            QtWidgets.QMessageBox.information(self, "Exception", str(e), QtWidgets.QMessageBox.Ok)
 
     def __create_statusbar(self):
-        status_bar = QWidget(self.centralWidget())
-        status_bar_layout = QHBoxLayout()
+        status_bar = QtWidgets.QWidget(self.centralWidget())
+        status_bar_layout = QtWidgets.QHBoxLayout()
         status_bar_layout.setContentsMargins(0, 0, 0, 0)
 
-        self.__label_infos = QLabel(status_bar)
+        self.__label_infos = QtWidgets.QLabel(status_bar)
         self.__label_infos.setAlignment(Qt.AlignLeft)
         status_bar_layout.addWidget(self.__label_infos)
         status_bar_layout.addStretch()
 
-        self.__label_version = QLabel(status_bar)
+        self.__label_version = QtWidgets.QLabel(status_bar)
         self.__label_version.setText("simple_live_qtwidgets v" + VERSION)
         self.__label_version.setAlignment(Qt.AlignRight)
         status_bar_layout.addWidget(self.__label_version)
 
-        self.__label_aboutqt = QLabel(status_bar)
+        self.__label_aboutqt = QtWidgets.QLabel(status_bar)
         self.__label_aboutqt.setObjectName("aboutQt")
         self.__label_aboutqt.setText("<a href='#aboutQt'>About Qt</a>")
         self.__label_aboutqt.setAlignment(Qt.AlignRight)
@@ -279,7 +279,7 @@ class MainWindow(QtWidgets.QMainWindow):
         """
         self.__label_infos.setText("Acquired: " + str(self.__frame_counter) + ", Errors: " + str(self.__error_counter))
 
-    @Slot()
+    #@Slot()
     def on_acquisition_timer(self):
         """
         This function gets called on every timeout of the acquisition timer
@@ -297,9 +297,9 @@ class MainWindow(QtWidgets.QMainWindow):
 
             # Get raw image data from converted image and construct a QImage from it
             image_np_array = converted_ipl_image.get_numpy_1D()
-            image = QImage(image_np_array,
+            image = QtWidgets.QImage(image_np_array,
                            converted_ipl_image.Width(), converted_ipl_image.Height(),
-                           QImage.Format_RGB32)
+                           QtWidgets.QImage.Format_RGB32)
 
             # Make an extra copy of the QImage to make sure that memory is copied and can't get overwritten later on
             image_cpy = image.copy()
@@ -317,10 +317,10 @@ class MainWindow(QtWidgets.QMainWindow):
         # Update counters
         self.update_counters()
 
-    @Slot(str)
+    #@Slot(str)
     def on_aboutqt_link_activated(self, link):
         if link == "#aboutQt":
-            QMessageBox.aboutQt(self, "About Qt")
+            QtWidgets.QMessageBox.aboutQt(self, "About Qt")
 if __name__ == '__main__':
     
     app = QtWidgets.QApplication([])
