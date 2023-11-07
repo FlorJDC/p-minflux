e21d7cf52eec208ecf29566564382f377a2c5519
florencia <florenciadchoque@gmail.com>
2023-11-07 10:10:48 -0300

    Tratando de armar un modulo externo para la camara IDS

---
+++ drivers/ids_cam.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  7 10:04:06 2023
+
+@author: Cibion
+"""
+from ids_peak import ids_peak
+from ids_peak_ipl import ids_peak_ipl
+from ids_peak import ids_peak_ipl_extension
+FPS_LIMIT = 30
+
+ids_peak.Library.Initialize()
+
